127487708ea865bfd4cc7ce01dc895839cbd2311
Ian Schreuder <ian.schreuder@gmail.com>
2017-07-14 19:06:18 -0600

    update ip

--- src/escrow.js
+++ src/escrow.js
@@ -91,7 +91,7 @@ window.addEventListener('load', function() {
   } else {
     console.log('No web3? You should consider trying MetaMask!')
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+    window.web3 = new Web3(new Web3.providers.HttpProvider("http://10.30.99.22:8545"));
   }
   setTimeout( () => App.startApp(), 300 );
 });
